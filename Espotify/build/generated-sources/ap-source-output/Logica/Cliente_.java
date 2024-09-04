cde6214efa331f0a1c7742f857105d191720c8fd
Ivan Dos Santos <102613357+AIVICODE@users.noreply.github.com>
2024-09-04 16:10:58 -0300

    un solo commit con todos mis cambios

---
+++ Espotify/build/generated-sources/ap-source-output/Logica/Cliente_.java
@@ -0,0 +1,11 @@
+package Logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-04T16:02:29", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ extends Usuario_ {
+
+
+}
