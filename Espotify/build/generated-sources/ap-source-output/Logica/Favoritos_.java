07a124e37e3205ef9a4ecbb03291e500e63db15d
Ivan Dos Santos <102613357+AIVICODE@users.noreply.github.com>
2024-09-06 16:52:06 -0300

    ok

---
+++ Espotify/build/generated-sources/ap-source-output/Logica/Favoritos_.java
@@ -0,0 +1,13 @@
+package Logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-06T16:51:00", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Favoritos.class)
+public class Favoritos_ { 
+
+    public static volatile SingularAttribute<Favoritos, Long> id;
+
+}
