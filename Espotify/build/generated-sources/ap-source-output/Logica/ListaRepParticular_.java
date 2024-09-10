716f1f2c6eba3b40b3ae7898b2ab0a21ed664005
madiayelen <madian.mayor@estudiantes.utec.edu.uy>
2024-09-10 16:07:46 -0300

    .

---
+++ Espotify/build/generated-sources/ap-source-output/Logica/ListaRepParticular_.java
@@ -0,0 +1,13 @@
+package Logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-10T16:06:22", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(ListaRepParticular.class)
+public class ListaRepParticular_ extends ListaRep_ {
+
+    public static volatile SingularAttribute<ListaRepParticular, Boolean> privada;
+
+}
