52e3868c9ba6dd3f3ae71d33dd89a5e0239c733f
madiayelen <madian.mayor@estudiantes.utec.edu.uy>
2024-09-18 15:12:31 -0300

    merge no ok

--- Espotify/build/generated-sources/ap-source-output/Logica/ListaRepGeneral_.java
+++ Espotify/build/generated-sources/ap-source-output/Logica/ListaRepGeneral_.java
@@ -3,7 +3,7 @@ package Logica;
 import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-18T15:06:40", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-18T15:10:47", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(ListaRepGeneral.class)
 public class ListaRepGeneral_ extends ListaRep_ {
 
